bf2863fc67b920989ad80e68b6b1b2645305cc04
LaneDoesThings <lane.wright2003@gmail.com>
2024-03-28 18:46:00 -0500

    test stupid

---
+++ test.s
@@ -0,0 +1,30 @@
+.global main
+
+main:
+    adr r0, thumb + 1
+    bx r0
+    .thumb
+    .syntax unified
+
+thumb:
+    mov r4, 50 @The initial value
+
+    ldr r0, =charInputMode
+    ldr r1, =charInput
+    bl scanf
+
+    ldr r1, =charInput
+    ldr r5, [r1]
+
+    cmp r5, #'S'
+    it eq
+    subeq r4, r4, #5
+
+    cmp r5, #'L'
+    it eq
+    subeq r4, r4, #10
+
+    mov r0, r4
+    bl printf
+
+    b thumb
